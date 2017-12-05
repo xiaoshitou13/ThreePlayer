a3e29fcea9827e9c832738befcfb2e27eefa67f6
15611616580 <739783051@qq.com>
2017-12-05 19:46:08 +0800

    gb

--- app/src/main/java/byc/by/com/threeplayer/find/FindFragment.java
+++ app/src/main/java/byc/by/com/threeplayer/find/FindFragment.java
@@ -2,17 +2,26 @@ package byc.by.com.threeplayer.find;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import byc.by.com.threeplayer.R;
 import byc.by.com.threeplayer.base.BaseFragment;
+import byc.by.com.threeplayer.find.adapter.MyAdapters;
+import byc.by.com.threeplayer.find.bean.FindBean;
+import byc.by.com.threeplayer.find.cardswipelayout.CardConfig;
+import byc.by.com.threeplayer.find.cardswipelayout.CardItemTouchHelperCallback;
+import byc.by.com.threeplayer.find.cardswipelayout.CardLayoutManager;
+import byc.by.com.threeplayer.find.cardswipelayout.OnSwipeListener;
 
 /**
  * Created by Zhang on 2017/12/5.
@@ -24,7 +33,7 @@ public class FindFragment extends BaseFragment {
     @BindView(R.id.huanyipi)
     Button huanyipi;
     Unbinder unbinder;
-
+    MyAdapters mAdapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -32,6 +41,62 @@ public class FindFragment extends BaseFragment {
 
         unbinder = ButterKnife.bind(this, v);
         return v;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    private void initData() {
+        mAdapter = new MyAdapters(getActivity(), list);
+        recyclers.setItemAnimator(new DefaultItemAnimator());
+        recyclers.setAdapter(mAdapter);
+        CardItemTouchHelperCallback cardCallback = new CardItemTouchHelperCallback(recyclers.getAdapter(), list);
+        cardCallback.setOnSwipedListener(new OnSwipeListener<FindBean>() {
+            @Override
+            public void onSwiping(RecyclerView.ViewHolder viewHolder, float ratio, int direction) {
+                MyAdapters.MyHolder myHolder = (MyAdapters.MyHolder) viewHolder;
+                viewHolder.itemView.setAlpha(1 - Math.abs(ratio) * 0.2f);
+                if (direction == CardConfig.SWIPING_LEFT) {
+                    myHolder.dislikeImageView.setAlpha(Math.abs(ratio));
+                } else if (direction == CardConfig.SWIPING_RIGHT) {
+                    myHolder.likeImageView.setAlpha(Math.abs(ratio));
+                } else {
+                    myHolder.dislikeImageView.setAlpha(0f);
+                    myHolder.likeImageView.setAlpha(0f);
+                }
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, FindBean mybean, int direction) {
+                MyAdapters.MyHolder myHolder = (MyAdapters.MyHolder) viewHolder;
+                viewHolder.itemView.setAlpha(1f);
+                myHolder.dislikeImageView.setAlpha(0f);
+                myHolder.likeImageView.setAlpha(0f);
+                //Toast.makeText(getActivity(), direction == CardConfig.SWIPED_LEFT ? "swiped left" : "swiped right", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSwipedClear() {
+                Toast.makeText(getActivity(), "data clear", Toast.LENGTH_SHORT).show();
+                recyclers.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        initData();
+                        recyclers.getAdapter().notifyDataSetChanged();
+                    }
+                }, 3000L);
+            }
+
+        });
+        final ItemTouchHelper touchHelper = new ItemTouchHelper(cardCallback);
+        final CardLayoutManager cardLayoutManager = new CardLayoutManager(recyclers, touchHelper);
+        recyclers.setLayoutManager(cardLayoutManager);
+        touchHelper.attachToRecyclerView(recyclers);
+
+
     }
 
     @Override
---
+++ app/src/main/java/byc/by/com/threeplayer/find/adapter/MyAdapters.java
@@ -0,0 +1,73 @@
+package byc.by.com.threeplayer.find.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import byc.by.com.threeplayer.R;
+import byc.by.com.threeplayer.find.bean.FindBean;
+
+/**
+ * date：2017/11/20
+ * 用途：
+ */
+public class MyAdapters extends RecyclerView.Adapter {
+    private Context mContext;
+    private ArrayList<FindBean> list;
+
+    public MyAdapters(Context mContext, ArrayList<FindBean> list) {
+        this.mContext = mContext;
+        this.list = list;
+    }
+
+    @Override
+    public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.iiiii, parent, false);
+        return new MyHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        MyHolder myHolder = (MyHolder) holder;
+        myHolder.description.setText(list.get(position).getRet().getList().get(position).getDescription());
+        myHolder.mTextView.setText(list.get(position).getRet().getList().get(position).getTitle());
+        Glide.with(mContext)
+                .load(list.get(position).getRet().getList().get(position).getPic())
+                .placeholder(R.mipmap.ic_launcher)
+                .crossFade()
+                .into(myHolder.img);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+
+    public class MyHolder extends RecyclerView.ViewHolder {
+        private TextView mTextView;
+        ImageView img;
+        TextView description;
+        public ImageView dislikeImageView;
+        public ImageView likeImageView;
+
+        public MyHolder(View itemView) {
+            super(itemView);
+            mTextView = itemView.findViewById(R.id.titless);
+            img = itemView.findViewById(R.id.imggg);
+            description = itemView.findViewById(R.id.description);
+            dislikeImageView=itemView.findViewById(R.id.iv_dislike);
+            likeImageView=itemView.findViewById(R.id.iv_like);
+        }
+
+
+    }
+}
