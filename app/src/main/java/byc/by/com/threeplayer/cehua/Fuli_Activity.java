d20e22893c5719e034a76628d439af92022289b1
Gmlde <1447312063@qq.com>
2017-12-06 16:12:01 +0800

    侧滑

---
+++ app/src/main/java/byc/by/com/threeplayer/cehua/Fuli_Activity.java
@@ -0,0 +1,15 @@
+package byc.by.com.threeplayer.cehua;
+
+import android.os.Bundle;
+
+import byc.by.com.threeplayer.R;
+import byc.by.com.threeplayer.base.BaseActivity;
+
+public class Fuli_Activity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fuli_);
+    }
+}
